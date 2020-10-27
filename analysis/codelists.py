001a93119307487da859fa41ff2e3a82a8566abc
Alex Walker <alex.walker@thedatalab.org>
2020-10-27 18:53:09 +0000

    additional creatinine codes

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -4,20 +4,22 @@ from cohortextractor import (
 )
 
 
-
-
-
-
 ckd_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-kidney-disease.csv", system="ctv3", column="CTV3ID",
+    "codelists/opensafely-chronic-kidney-disease.csv",
+    system="ctv3",
+    column="CTV3ID",
 )
 
 atrial_fibrillation_codes = codelist_from_csv(
-    "codelists/opensafely-atrial-fibrillation-clinical-finding.csv", system="ctv3", column="CTV3Code",
+    "codelists/opensafely-atrial-fibrillation-clinical-finding.csv",
+    system="ctv3",
+    column="CTV3Code",
 )
 
 doac_contraindication_codes = codelist_from_csv(
-    "codelists/opensafely-explicit-contraindication-to-doacs-direct-acting-anticoagulants.csv", system="ctv3", column="id",
+    "codelists/opensafely-explicit-contraindication-to-doacs-direct-acting-anticoagulants.csv",
+    system="ctv3",
+    column="id",
 )
 
 # DEMOGRAPHIC CODELIST
@@ -30,24 +32,62 @@ ethnicity_codes = codelist_from_csv(
 
 # MEDICATIONS
 warfarin_codes = codelist_from_csv(
-    "codelists/opensafely-warfarin.csv", system="snomed", column="id",
+    "codelists/opensafely-warfarin.csv",
+    system="snomed",
+    column="id",
 )
 
 doac_codes = codelist_from_csv(
-    "codelists/opensafely-direct-acting-oral-anticoagulants-doac.csv", system="snomed", column="id",
+    "codelists/opensafely-direct-acting-oral-anticoagulants-doac.csv",
+    system="snomed",
+    column="id",
 )
 
 ## TESTS
 inr_codes = codelist_from_csv(
-    "codelists/opensafely-international-normalised-ratio-inr.csv", system="ctv3", column="id",
+    "codelists/opensafely-international-normalised-ratio-inr.csv",
+    system="ctv3",
+    column="id",
 )
 
 high_inr_codes = codelist_from_csv(
-    "codelists/opensafely-high-international-normalised-ratio-inr.csv", system="ctv3", column="id",
+    "codelists/opensafely-high-international-normalised-ratio-inr.csv",
+    system="ctv3",
+    column="id",
 )
 
 ttr_codes = codelist(["Xaa68"], system="ctv3")
 
-renal_function_test_codes = codelist(["451..","XE2q5","XacUK"], system="ctv3")
+renal_function_test_codes = codelist(
+    [
+        "451..",
+        "44J3.",
+        "44J3z",
+        "4I37.",
+        "X771Q",
+        "X80D7",
+        "XE26a",
+        "XE2q5",
+        "XaERX",
+        "XaERc",
+        "XaETQ",
+        "XacUK",
+    ],
+    system="ctv3",
+)
 
-creatinine_codes = codelist(["XE2q5"], system="ctv3")
+creatinine_codes = codelist(
+    [
+        "44J3.",
+        "44J3z",
+        "4I37.",
+        "X771Q",
+        "X80D7",
+        "XE26a",
+        "XE2q5",
+        "XaERX",
+        "XaERc",
+        "XaETQ",
+    ],
+    system="ctv3",
+)
